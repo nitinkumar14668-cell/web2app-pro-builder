5b6c1f71e3ae2aca646940d48045f10d337bb5f0
nitinkumar14668-cell <nitinkumar14668@gmail.com>
2026-02-02 19:40:26 +0530

    Update settings.gradle.kts

--- web2app-pro-builder/android-template/settings.gradle.kts
+++ web2app-pro-builder/android-template/settings.gradle.kts
@@ -1,1 +1,18 @@
-test
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Web2AppPro"
+include(":app")
