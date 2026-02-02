c88e0df97b3a05c1b18bc1451ab57536a9095e49
nitinkumar14668-cell <nitinkumar14668@gmail.com>
2026-02-02 19:43:08 +0530

    Update build.gradle.kts

--- web2app-pro-builder/android-template/app/build.gradle.kts
+++ web2app-pro-builder/android-template/app/build.gradle.kts
@@ -1,1 +1,31 @@
-test
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.web2apppro"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.web2apppro"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+
+    // WebView uses built-in android.webkit.WebView
+}
