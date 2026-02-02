96c6b9c8bbaa9d1b7deb8b5935fcbaf525da2deb
nitinkumar14668-cell <nitinkumar14668@gmail.com>
2026-02-02 19:45:47 +0530

    Update MainActivity.kt

--- web2app-pro-builder/android-template/app/src/main/java/com/web2apppro/MainActivity.kt
+++ web2app-pro-builder/android-template/app/src/main/java/com/web2apppro/MainActivity.kt
@@ -1,1 +1,48 @@
-test
+package com.web2apppro
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webview)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.allowFileAccess = true
+        webView.settings.allowContentAccess = true
+
+        webView.webChromeClient = WebChromeClient()
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                return false
+            }
+        }
+
+        // Load uploaded website
+        webView.loadUrl("file:///android_asset/www/index.html")
+    }
+
+    override fun onBackPressed() {
+        if (::webView.isInitialized && webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
