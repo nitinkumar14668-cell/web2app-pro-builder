52f267d440e6fe022a814ee322cd798b5f7f27da
nitinkumar14668-cell <nitinkumar14668@gmail.com>
2026-02-02 20:43:41 +0530

    Update build.gradle.kts

--- web2app-pro-builder/android-template/app/build.gradle.kts
+++ web2app-pro-builder/android-template/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    id("org.jetbrains.kotlin.android")
+    kotlin("android")
 }
 
 android {
@@ -13,19 +13,43 @@ android {
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+
+        multiDexEnabled = true
     }
 
     buildTypes {
         release {
             isMinifyEnabled = false
-            isShrinkResources = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
+        debug {
+            isMinifyEnabled = false
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
 
-    // WebView uses built-in android.webkit.WebView
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
+    // Google Mobile Ads SDK (AdMob)
+    implementation("com.google.android.gms:play-services-ads:23.0.0")
 }
